4e54b30e644534085b1841d0d1cc6a72d584aac7
Anton Sankov <a.sankov96@gmail.com>
2023-03-04 00:09:23 +0200

    Refactor the app
    - extract the whole deletion logic into a function
    and call that via a `select` over a time channel,
    this will make the app wait every time after it tries to delete a pod
    no matter whether the deletion succeeded or not.

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"crypto/rand"
 	"fmt"
 	"math/big"
+	"os"
+	"os/signal"
 	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -23,63 +25,66 @@ import (
 	"github.com/asankov/k8s-chaos-monkey/config"
 )
 
-func run() error {
+func main() {
 	// Create an in-cluster kubeConfig for Kubernetes.
 	// This will fail if our workload is not running inside a Kubernetes cluster
 	// but for now it should be fine, since we expect that we are always running inside Kubernetes.
 	kubeConfig, err := rest.InClusterConfig()
 	if err != nil {
-		return err
+		return
 	}
 	clientset, err := kubernetes.NewForConfig(kubeConfig)
 	if err != nil {
-		return err
+		return
 	}
 
 	cfg, err := config.NewConfigFromEnv()
 	if err != nil {
-		return err
+		return
 	}
+
+	cancelSignal := make(chan os.Signal)
+	signal.Notify(cancelSignal, os.Interrupt, os.Kill)
 
 	for {
-		// List all pods in the given namespace.
-		// If we want to have some sort of timeout for this API call we can create a new TimeoutContext via context.WithTimeout.
-		pods, err := clientset.CoreV1().Pods(cfg.Namespace).List(context.Background(), metav1.ListOptions{})
-		if err != nil {
-			fmt.Printf("[ERROR] Error while listing Pods: %v", err)
-			// Continuing here will make another API call without waiting for the period to pass.
-			// We might want to check the type of the error, or implement some sort of exponential back-off
-			// to prevent the Kubernetes API being overloaded by requests.
-			continue
+		select {
+		case <-time.After(time.Duration(cfg.PeriodInSeconds) * time.Second):
+			if err := deletePod(clientset, cfg); err != nil {
+				fmt.Printf("[ERROR]: %v", err)
+			}
+		case <-cancelSignal:
+			fmt.Printf("Detected cancel signal, stopping application")
+			return
 		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-		// Choose a random pod to be deleted.
-		// Use crypto/rand, because math/rand produces deterministic results and should not be used in production code.
-		i, err := rand.Int(rand.Reader, big.NewInt(int64(len(pods.Items))))
-		if err != nil {
-			fmt.Printf("[ERROR] Error while generating random number: %v", err)
-			// Continuing here will make another API call without waiting for the period to pass.
-			// We might want to check the type of the error, or implement some sort of exponential back-off
-			// to prevent the Kubernetes API being overloaded by requests.
-			continue
-		}
-		podToBeDeleted := pods.Items[i.Int64()]
-
-		fmt.Printf("Chose to delete pod [%v]", podToBeDeleted.Name)
-
-		// Delete the pod.
-		// If we want to have some sort of timeout for this API call we can create a new TimeoutContext via context.WithTimeout.
-		if err := clientset.CoreV1().Pods(cfg.Namespace).Delete(context.Background(), podToBeDeleted.Name, metav1.DeleteOptions{}); err != nil {
-			fmt.Printf("[ERROR] Error while deleting pod [%v]: %v", podToBeDeleted.Name, err)
-			// Continuing here will make another API call without waiting for the period to pass.
-			// We might want to check the type of the error, or implement some sort of exponential back-off
-			// to prevent the Kubernetes API being overloaded by requests.
-			continue
-		}
-
-		fmt.Printf("Succesfully deleted the pod [%v]", podToBeDeleted.Name)
-
-		time.Sleep(time.Duration(cfg.PeriodInSeconds) * time.Second)
 	}
+}
+
+func deletePod(clientset *kubernetes.Clientset, cfg *config.Config) error {
+	// List all pods in the given namespace.
+	// If we want to have some sort of timeout for this API call we can create a new TimeoutContext via context.WithTimeout.
+	pods, err := clientset.CoreV1().Pods(cfg.Namespace).List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("error while listing Pods: %w", err)
+	}
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+	// Choose a random pod to be deleted.
+	// Use crypto/rand, because math/rand produces deterministic results and should not be used in production code.
+	i, err := rand.Int(rand.Reader, big.NewInt(int64(len(pods.Items))))
+	if err != nil {
+		return fmt.Errorf("error while generating random number: %w", err)
+	}
+	podToBeDeleted := pods.Items[i.Int64()]
+
+	fmt.Printf("Chose to delete pod [%v]", podToBeDeleted.Name)
+
+	// Delete the pod.
+	// If we want to have some sort of timeout for this API call we can create a new TimeoutContext via context.WithTimeout.
+	if err := clientset.CoreV1().Pods(cfg.Namespace).Delete(context.Background(), podToBeDeleted.Name, metav1.DeleteOptions{}); err != nil {
+		return fmt.Errorf("error while deleting pod [%v]: %w", podToBeDeleted.Name, err)
+	}
+
+	fmt.Printf("Succesfully deleted the pod [%v]", podToBeDeleted.Name)
+
+	return nil
 }
